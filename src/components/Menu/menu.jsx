eb1d99e6c5a3c38375e1dea5179333abeac21fdc
HuyTo1368 <huyto1368@gmail.com>
2021-12-14 21:18:32 +0700

    Lan 2

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import './menu.css'
 
-const tabs = ['Tiến độ điều tra', 'Phân tích số liệu', 'Danh sách dân số', 'Tra cứu thông tin']
+const tabs = ['Tiến độ điều tra', 'Phân tích số liệu', 'Danh sách dân số', 'Tra cứu thông tin', 'Cấp tài khoản']
 
 export default function Menu(props) {
     return (
         <div>
             {tabs.map(tab => (
-                <div className="TanLong"
-                    key={tab}
+                <div 
+                key={tab}
+                className="menu1"
                 >
-                <a href="">{tab}</a>
-                    
+                    <button className="active" href="#home">
+                        {tab}
+                    </button>
                 </div>
             ))}
         </div>
--- src/components/tranghai/Trangchinh.jsx
+++ src/components/tranghai/Trangchinh.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Menu from "../Menu/menu";
+import './tranghai.css'
 
 const tabs = ['Tiến độ điều tra', 'Phân tích số liệu', 'Danh sách dân số', 'Tra cứu thông tin']
 
 export default function Tranghai(props) {
     return (
-        <div>
+        <div className="display">
             <Menu />
 
             <div>
-                <img 
-                    src={'https://hanoispiritofplace.com/wp-content/uploads/2016/01/hinh-mat-cuoi-11.jpg'}
-                />
+                
             </div>
         </div>
     )
