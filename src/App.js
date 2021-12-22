8fedb2f608d8b744517f7860860e1ff963cbd9ed
Đỗ Quang Huynh <doquanghuynh2911@gmail.com>
2021-12-22 10:13:55 +0700

    Huynh router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router";
+
 import Trangdau from "./components/trangdau/Trangdau.jsx";
 import Tranghai from "./components/tranghai/Trangchinh.jsx";
-import { Routes, Route } from "react-router";
 import { Tiendo } from "./components/tiendodieutra/tiendodieutra";
 import PhanTich from "./components/phantichsolieu/phantichsolieu";
 import Member from "./components/member/Member";
 import AddMember from "./components/member/AddMember";
-// import BarChart from './components/ChartPie/ChartPie';
+import EnterData from "./components/enter_data/EnterData";
+import "./App.css";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route path="Member" element={<Member />} />
               <Route path="Phantich" element={<PhanTich />} />
               <Route path="Member/addMember" element={<AddMember />} />
+              <Route path="Nhaplieu" element={<EnterData/>} />
           </Route>
         </Routes>
       </>
--- src/components/member/AddMember.js
+++ src/components/member/AddMember.js
@@ -3,38 +3,63 @@ import { Link } from "react-router-dom";
 import { Button, Alert } from "react-bootstrap";
 import styte from "./Member.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import './AddMember.css'
 
 export default function AddMember() {
+
   return (
     <div>
-      <form>
+      <br/>
+      <h1>Tạo tài khoản cho A1</h1>
+      <br/><br/><br/>
+
+      <form className="containerr">
         <div className="form-group row">
           <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
-            Email
+            Tài khoản
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readOnly
-              className="form-control-plaintext"
+              className="form-control"
               id="staticEmail"
-              defaultValue="email@example.com"
+              required
+              placeholder="id"
             />
           </div>
         </div>
+        <br></br>
         <div className="form-group row">
           <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
-            Password
+            Mật khẩu
           </label>
           <div className="col-sm-10">
             <input
               type="password"
               className="form-control"
+              required
               id="inputPassword"
               placeholder="Password"
             />
           </div>
         </div>
+        <br/>
+        <div className="form-group row">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+            Nhập lại
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control"
+              required
+              id="inputPassword"
+              placeholder="Enter Password again"
+            />
+          </div>
+        </div>
+        <br/><br/>
+        <button type="submit" class="btn btn-primary">Xác nhận</button>
       </form>
     </div>
   );
