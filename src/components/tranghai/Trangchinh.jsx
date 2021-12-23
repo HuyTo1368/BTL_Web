e634961507da3fa924e32571e757edbceccddb2f
HuyTo1368 <huyto1368@gmail.com>
2021-12-23 15:16:40 +0700

    1

--- src/components/tranghai/Trangchinh.jsx
+++ src/components/tranghai/Trangchinh.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes,Outlet } from "react-router-dom";
 import Menu from "../public/Menu/menu";
-import Navbar from "../public/Navbar/Navbar";
+import Navbar from "../public/Navbar/Navbar"
 import Footer from "../public/footer/footer";
 import "./tranghai.css";
 import { Tiendo } from "../tiendodieutra/tiendodieutra";
 import { useState } from "react";
 import PhanTich from "../phantichsolieu/phantichsolieu";
-
 import Trangdau from "../trangdau/Trangdau";
 import AddMember from "../member/AddMember";
 import Member from "../member/Member";
